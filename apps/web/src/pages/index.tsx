38f26fe6938bc13a17964877e244b91577bb185e
Akshit1311 <a4748g@gmail.com>
2022-11-15 17:40:39 +0530

    convert FileList to File[]

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -2,12 +2,12 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { ChangeEvent } from "react";
-// import { storeFiles } from "../utils/store";
+import { storeFiles } from "../utils/store";
 
 const Home: NextPage = () => {
   const storeToFil = (e: ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.files);
-    // storeFiles(e.target.files)
+    if (e.target.files) storeFiles(Array.from(e.target.files));
   };
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-gray-500">
