bc3dcc317d8550bd5095a9a364e5871f904321f0
anjanaaaaaaaa <thanjana7@gmail.com>
2022-11-19 01:38:48 +0530

    add: deploy script

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+const { ALCHEMY_RPC_URL, YOUR_PRIV_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  paths: { artifacts: "./src/artifacts" },
+  networks: {
+    matic: {
+      url: ALCHEMY_RPC_URL,
+      accounts: [YOUR_PRIV_KEY],
+    },
+  },
 };
